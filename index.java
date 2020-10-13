d3f018b87924e4798a1f600d6dc725a19373603a
kokooldude <khalileoliver@gmail.com>
2020-10-13 15:21:49 +0000

    I did it!

--- index.java
+++ index.java
@@ -1,8 +1,19 @@
+int x = 150;
+int y = 150;
+int w = 50;
+int h = 50;
+
+
 void setup() {
 	size(500, 500);
 }
 
 void draw() {
+
+
+
+
+
 // Step 1: Write code that draws the screenshot in the files 
 // with hard-coded values. (Feel free to use colors 
 // instead of grayscale.)
@@ -18,6 +29,18 @@ void draw() {
   background(255);
   stroke(0);
   fill(175);
+  ellipse(x, y, w, h)
+  ellipse(x + 100, y, w, h)
+  ellipse(x, y + 100, w, h)
+  ellipse(x + 100, y + 100, w, h)
+
+
+
+
+
+
+
+
 
 
   
